9e421e5c353c5cb71a4eb37faf48b2846dffb0a3
Andrei <soprachev@mail.ru>
2022-07-11 06:33:16 +0300

    upd cache

--- app/routes/loadMap.js
+++ app/routes/loadMap.js
@@ -20,39 +20,63 @@ export default async function routes(fastify) {
   fastify.get('/map/:id', async (req, res) => {
     const mapID = req.params.id
 
+    if (ceche[mapID]?.loading) { await ceche[mapID].loading }
+
     const cachedMap = ceche[mapID]
     const cachedMapTime = cachedMap?.time || 0
     const delta = (Date.now() - cachedMapTime) / 1000
+    var cacheLoadingPromise = {}
 
-    if (cachedMap && (delta < cacheTimeout || (await Map.findOne({ mapID: mapID }, { __v: true })).__v == cachedMap.__v)) {
+    if (cachedMap) {
+      if (delta < cacheTimeout) {
+        return sendGziped(res, cachedMap.map)
+      }
 
-      if (delta > cacheTimeout) {
+      cachedMap.loading = new Promise((resolve, reject) => cacheLoadingPromise = { resolve, reject })
+      const mapVersion = await Map.findOne({ mapID: mapID }, { __v: true })
+
+      if (mapVersion.__v == cachedMap.__v) {
         cachedMap.time = Date.now()
+        cachedMap.loading = null
+        cacheLoadingPromise.resolve()
+        return sendGziped(res, cachedMap.map)
       }
-
-      sendGziped(res, cachedMap.map)
     }
 
+
+
+    var loadingPromise = {}
+
+    ceche[mapID] = {
+      loading: new Promise((resolve, reject) => loadingPromise = { resolve, reject })
+    }
+
+    console.log("> Loading map " + mapID);
     const map = await Map.findOne({ mapID: mapID })
+    console.log("< Loading map " + mapID);
 
-    if (map) {
-      const result = {
-        imdf: map.imdf,
-        meta: { title: "TODO" }
-      }
-
-      const gziped = await gzip(Buffer.from(JSON.stringify(result)))
-
-      sendGziped(res, gziped)
-
-      return ceche[mapID] = {
-        map: gziped,
-        __v: map.__v,
-        time: Date.now()
-      }
+    if (!map) {
+      loadingPromise.reject()
+      cacheLoadingPromise.reject?.()
+      return res.code(404).send()
     }
 
-    res.code(404).send()
+    const result = {
+      imdf: map.imdf,
+      meta: { title: "TODO" }
+    }
 
+    const gziped = await gzip(Buffer.from(JSON.stringify(result)))
+
+    sendGziped(res, gziped)
+
+    ceche[mapID] = {
+      map: gziped,
+      __v: map.__v,
+      time: Date.now()
+    }
+
+    loadingPromise.resolve()
+    cacheLoadingPromise.resolve?.()
   })
 }
